7e12e691300a8418132e3c3f9ca7e6d2e4af3498
binb.wen@o-ibp.com <binb.wen@o-ibp.com>
2020-12-22 15:03:34 +0800

    zk

---
+++ cloud-zookeeper-client/src/test/java/com/cloud/learn/CloudZookeeperClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudZookeeperClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
