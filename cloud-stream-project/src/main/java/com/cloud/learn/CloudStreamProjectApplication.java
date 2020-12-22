1741f74a9b36c92d623ecfffb1de9abacf721186
binb.wen@o-ibp.com <binb.wen@o-ibp.com>
2020-12-22 15:22:44 +0800

    stream

---
+++ cloud-stream-project/src/main/java/com/cloud/learn/CloudStreamProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudStreamProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudStreamProjectApplication.class, args);
+    }
+
+}
