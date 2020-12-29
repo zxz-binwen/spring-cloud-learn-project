c5af918bdb255d0086efc01be82c1afba64f3cb8
wbb <oibp@1234>
2020-12-29 19:26:01 +0800

    commit

--- cloud-eureka-server/src/main/java/com/cloud/learn/CloudEurekaServerApplication.java
+++ cloud-eureka-server/src/main/java/com/cloud/learn/CloudEurekaServerApplication.java
@@ -4,11 +4,33 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 @EnableEurekaServer
 @SpringBootApplication
 public class CloudEurekaServerApplication {
 
+    private static  final ExecutorService executorService = Executors.newSingleThreadExecutor();
+
     public static void main(String[] args) {
+
+        Future<?> future = executorService.submit(() -> {
+
+        });
+
+        try {
+            Object o = future.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } finally {
+            future.cancel(true);
+        }
+
         SpringApplication.run(CloudEurekaServerApplication.class, args);
     }
 
---
+++ spring-security/src/test/java/com/cloud/learn/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
